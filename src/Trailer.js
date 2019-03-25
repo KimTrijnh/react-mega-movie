9647cabe0c72e3c6fd1bf2c9ab1dea0d86109db2
M.T <bmthang94@gmail.com>
2019-03-25 15:49:32 +0700

    add list of trailers into modal

--- src/Trailer.js
+++ src/Trailer.js
@@ -4,34 +4,57 @@ import YouTube from '@u-wave/react-youtube';
 export default class Trailer extends Component {
     constructor(props) {
         super(props);
-        this.state = { trailer: {} };
+        this.state = {
+            trailers: [],
+            currentTrailer: {},
+        };
         this.componentDidMount = this.componentDidMount.bind(this);
     }
 
     componentDidMount() {
-        console.log(this.props.movieId);
         const API_KEY = "f55e42fa6f08a80dc89685d49eab586d";
         const TrailerAPI = `https://api.themoviedb.org/3/movie/${this.props.movieId}/videos?api_key=${API_KEY}`;
 
         fetch(TrailerAPI)
             .then(response => response.json())
-            .then(data => {
+            .then(async data => {
                 console.log(data)
-                this.setState({
-                    trailer: data.results.find(ele => {
+                await this.setState({
+                    trailers: data.results.filter(ele => {
                         return ele.type === 'Trailer'
                     })
                 });
+                await this.setState({
+                    currentTrailer: this.state.trailers[0]
+                }, () => console.log(this.state.currentTrailer));
             });
     }
-
+    chooseTrailer = index => {
+        this.setState({
+            currentTrailer: this.state.trailers[index]
+        })
+    }
     render() {
         return (
-            <div className="videoWrapper">
-                <YouTube
-                    video={this.state.trailer.key}
-                    autoplay
-                />
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-12 col-sm-5">
+                        <ul>
+                            {this.state.trailers.map((trailer, index) => (
+                                <p key={index}>{index +1}. <a href="#" onClick={() => this.chooseTrailer(index)}>{trailer.name}</a></p>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="col-12 col-sm-7">
+                        <div className="videoWrapper">
+                            <YouTube
+                                video={this.state.currentTrailer.key}
+                                autoplay
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
